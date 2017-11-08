e8ef50b383f6f6f68aef8b2eaa2f4669d7b61cd6
jordaninthewind <jordan.kline@gmail.com>
2017-11-08 23:20:52 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,9 +12,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count == 0
+  if turn_count(board) == 0
     return "X"
-  elsif turn_count % 2 == 0
+  elsif turn_count(board) % 2 == 0
     return "X"
   else
     return "O"
